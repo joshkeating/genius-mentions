03a8bf7c5f98f1c0e1f5a456314b822e7b4eee18
joshkeating <josh.keating7@gmail.com>
2018-08-21 18:46:44 -0700

    got the json obj for a random artist

--- process-artists.py
+++ process-artists.py
@@ -1,6 +1,36 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import re
+import json
+import urllib.request
+
+# get access tokens from file
+with open('./secrets.json') as secrets:
+    secretKeys = json.load(secrets)
+
+
+targetArtist = "21 Savage"
+
+token = secretKeys["access_token"]
+
+
+# print(token)
+
+
+host = "https://api.genius.com/search?q="
+
+query = host + urllib.request.quote(targetArtist)
+request = urllib.request.Request(query)
+
+request.add_header("Authorization", "Bearer " + token)
+request.add_header("User-Agent", "")
+
+
+response = json.load(urllib.request.urlopen(request))
+
+# give a big json obj of the artist
+print(response)
+
 
 
 
