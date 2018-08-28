f9abb26f107b3ce85c29e66aa500042dd131fee4
joshkeating <josh.keating7@gmail.com>
2018-08-27 19:25:15 -0700

    scraping code done

--- pull-lyrics.py
+++ pull-lyrics.py
@@ -2,45 +2,97 @@ from bs4 import BeautifulSoup as bs
 import requests
 import csv
 import re
-
-# scrape for lyrics and metadata
-
-csvfile =  open('./data/output/ao1.csv', newline='')
-reader = csv.reader(csvfile, delimiter=';', quotechar='|')
-
-i = 0
-# move the iterator from the first line
-next(reader)
+import random
+import time
 
 
-pattern = r"\[.*\]|\(x[0-9]\)|\n"
+def checkElementExistence(input):
+    if input is None:
+        return "NA"
+    else:
+        return input.get_text()
 
-while i < 2:
+def processNRows(sourceFile, outputFile):
 
-    currentRow = next(reader)
+    sourceFilePath = "./data/output/" + sourceFile
+    outputFilePath = "./data/output/" + outputFile
 
-    songTitle = currentRow[1]
-    artist = currentRow[4]
-    url = currentRow[5]
+    # open input file
+    csvfile =  open(sourceFilePath, newline='')
+    reader = csv.reader(csvfile, delimiter=';', quotechar='|')
 
-    targetPage = requests.get(url)
-    html = bs(targetPage.text, "html.parser")
-
-    lyricsStandard = html.find("div", class_="lyrics").get_text()
-
-    lyrics = re.sub(pattern, " ", lyricsStandard)
-
-    print("Artist: " + artist)
-    print("Song Title: " + songTitle)
-    print(lyrics)
-    print()
-
-    i += 1
+    # open file to write updated results 
+    output = open(outputFilePath, 'w', newline='')
+    writer = csv.writer(output, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
 
-    
+    # add the header to the file
+    writer.writerow(["song_id", "title", "title_with_featured", "primary_artist_id",
+                     "primary_artist_name", "url", "album", "full_date", "date_month", "date_year", "lyrics"])
 
-  
+    count = 0
 
-    
+    # define the regex to replace metadata we dont care about
+    pattern = r"\[.*\]|\(x[0-9]\)|\n"
+
+    # skip the header
+    next(reader)
+
+    for currentRow in reader:
+
+        try:
+
+            # get data from file
+            songId = currentRow[0]
+            songTitle = currentRow[1]
+            titleWithFeat = currentRow[2]
+            primaryArtistId = currentRow[3]
+            artist = currentRow[4]
+            url = currentRow[5]
+
+
+            targetPage = requests.get(url)
+            html = bs(targetPage.text, "html.parser")
+
+
+            dateMonth = "NA"
+            dateYear = "NA"
+
+            # get raw text from a couple of elements on the page
+            lyricsStandard = checkElementExistence(html.find("div", class_="lyrics"))
+            fullDate = checkElementExistence(html.find("span", class_="metadata_unit-info metadata_unit-info--text_only")) 
+            album = checkElementExistence(html.find("a", class_="song_album-info-title")).strip()
+
+            # process metadata
+            if fullDate != "NA":
+                splitDate = fullDate.split()
+                dateMonth = splitDate[0]
+                dateYear = splitDate[2]
+                
+            # remove annotations and extra spaces
+            lyricsTemp = re.sub(pattern, " ", lyricsStandard)
+            lyrics = re.sub(' +',' ',lyricsTemp)
+
+            # write to file
+            writer.writerow([songId, songTitle, titleWithFeat, primaryArtistId, artist, url, album, fullDate, dateMonth, dateYear, lyrics])
+            count += 1
+
+            # print status and sleep for random intervals
+            if count % 10 == 0:
+                print(str(count) + " records processed")
+                waitTime = random.randint(4, 15)
+                print("Waiting " + str(waitTime) + " seconds...")
+                time.sleep(waitTime)
+        
+        except IndexError:
+            print("Malformed html data")
+        except:
+            print("Something Broke!")
+
+    return
+
+        
+# run it
+processNRows("ao1.csv", "full-ao1.csv")
+
 
