5500bc759166bba66d5c44ef00c58b5282179dc0
joshkeating <josh.keating7@gmail.com>
2018-09-03 11:39:23 -0700

    plot me a scatter

--- viz.py
+++ viz.py
@@ -1,9 +1,16 @@
 import csv
 import re
 from bokeh.plotting import figure, output_file, show
+from datetime import datetime
 
 
-sourceFilePath = "./data/output/full-ao1.csv"
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import *
+from bokeh.transform import factor_cmap, jitter
+
+
+
+sourceFilePath = "./data/output/all.csv"
 
 # open input file
 csvfile =  open(sourceFilePath, newline='')
@@ -23,35 +30,43 @@ def searchMention(lyrics, target):
 
 print("searching for " + target)
 
-dateFormat = re.compile(r'\d{4}')
+dateFormat = re.compile(r'[a-zA-Z]+ [0-9]+, \d{4}')
 
 
 oc = []
 dates = []
+artists = []
 
 for currentRow in reader:
 
     # songTitle = currentRow[1]
     # artist = currentRow[4]
 
-    year = currentRow[9]
+    dateRaw = currentRow[7]
 
 
     count = searchMention(currentRow[10], target)
 
-    
-    
+    # dateFormat.match(year)
 
-    if count != 0 and dateFormat.match(year):
+    if count != 0 and count < 30 and dateFormat.match(dateRaw):
+
+        formattedDates = datetime.strptime(dateRaw, '%B %d, %Y')
+        artists.append(currentRow[4])
         oc.append(count)
-        dates.append(year)
+        dates.append(formattedDates)
 
         # print(str(artist) + ",  " + str(songTitle) + ",  " + "occurrences:  " + str(count))
         # totalCount += count
 
 
-# print(oc)
-# print(dates)
+# print(len(oc))
+# print(len(dates))
+
+# print(artists)
+
+# print(len(dates))
+# print(len(set(dates)))
 
 # print()
 # print(totalCount)
@@ -60,16 +75,28 @@ for currentRow in reader:
 # what I want: 
 # scatterplot with line, hoverable metadata
 
-
+genPal = viridis(len(artists))
 
 # output to static HTML file
 output_file("./plots/lines.html")
 
-# create a new plot with a title and axis labels
-p = figure(title="title", x_axis_label='years', y_axis_label='counts')
+source1 = ColumnDataSource(data=dict(dates=dates, counts=oc, colors=artists))
 
-# add a line renderer with legend and line thickness
-p.circle(dates, oc, fill_alpha=0.2, size=oc, legend="Temp.")
+# # create a new plot with a title and axis labels
+p = figure(title="title", x_axis_label='time', y_axis_label='counts', x_axis_type="datetime")
+p.circle(x='dates', y=jitter('counts', width=0.6, range=p.y_range), fill_alpha=0.6, size=10, source=source1, fill_color=factor_cmap('colors', palette=genPal, factors=artists))
+
+
+
+# source2 = ColumnDataSource(data=dict(dates=dates, counts=oc, colors=artists))
+# p2 = figure(title="title", x_axis_type="datetime")
+# p2.vbar(x='dates', top='counts', width=0.9, source=source2, fill_color=factor_cmap('colors', palette=genPal, factors=artists))
+
+
+
+# p.line(dates, oc)
 
 # show the results
+# show(p)
+
 show(p)
