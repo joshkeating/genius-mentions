dde88730c71a249397abc5d744aa026108e3e39e
joshkeating <josh.keating7@gmail.com>
2018-10-05 04:47:37 -0400

    added pandas df manipulations

--- viz-sql.py
+++ viz-sql.py
@@ -12,7 +12,7 @@ from bokeh.transform import factor_cmap, jitter
 
 
 
-def searchMention(lyrics, target_word):
+def search_mention(lyrics, target_word):
     """ 
     Takes in a lyrics string and a target word, returns a count of the number of times the target
     word occurs in the lyric string
@@ -20,6 +20,19 @@ def searchMention(lyrics, target_word):
 
     return sum(1 for n in re.finditer(r'\b%s\b' % re.escape(target_word), lyrics, flags=re.IGNORECASE))
 
+
+def convert_to_datetime(input_string):
+    """
+    docstring
+    """
+
+    if re.match(r'[a-zA-Z]+ [0-9]+, \d{4}', input_string):
+        return datetime.strptime(input_string, '%B %d, %Y')
+    else:
+        return None
+
+
+    
 
 def month_plot(target_word):
     """
@@ -33,13 +46,24 @@ def month_plot(target_word):
 
     conn = sqlite3.connect(database_connection_string)
 
-    # this might be bad for performance in the long run
-    full_df = pd.read_sql_query("SELECT * FROM songs AS S, artists AS A WHERE S.primary_artist_id = A.artist_id", conn)
+    query_string = '''SELECT S.title, A.artist_name, S.album, S.full_date, S.lyrics
+                        FROM songs AS S, artists AS A
+                        WHERE S.primary_artist_id = A.artist_id'''
 
-    print(full_df.head())
+    # this might be bad for performance in the long run
+    full_df = pd.read_sql_query(query_string, conn)
+    # add count of target word to df
+    full_df['count'] = full_df['lyrics'].apply(search_mention, args=(target_word,))
+
+    # convert date string to datetime object
+    full_df['full_date'] = full_df['full_date'].apply(convert_to_datetime)
+
+    # print(full_df.head())
+    # print(list(full_df))
+    # print(full_df['full_date'])
 
     return
 
 
 
-month_plot("word")
+month_plot("patek")
